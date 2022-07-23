c42fea4de41329e6593b5bbb7229a2cb302f09bb
Donald Pakkies <donaldpakkies@gmail.com>
2022-07-23 19:29:55 +0200

    feat: add typescript publishables

---
+++ formidable/vendor/ts/routes/web.ts
@@ -0,0 +1,22 @@
+import { Inertia } from '@formidablejs/inertia';
+import { Request } from '@formidablejs/framework';
+import { Route } from '@formidablejs/framework';
+
+/**
+ * --------------------------------------------------------------------------
+ * Web Routes
+ * --------------------------------------------------------------------------
+ *
+ * Here is where you can register web routes for your application. These
+ * routes are loaded by the RouteServiceResolver within a group which
+ * is assigned the "session" middleware group.
+ */
+
+Route.get('/', (request: Request) => {
+	Inertia.render('Welcome', {
+		formidableVersion: request.version,
+		nodeVersion: process.version
+	}).withViewData({
+		locale: request.locale!
+	});
+});
