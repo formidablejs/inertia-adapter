9ee78bc2ee5b1b3c0cc9324b05e098de3bb41ee1
Donald Pakkies <donaldpakkies@gmail.com>
2022-11-24 20:17:46 +0200

    feat: add mix config option

--- formidable/vendor/imba/config/inertia.imba
+++ formidable/vendor/imba/config/inertia.imba
@@ -10,4 +10,13 @@ export default {
 
 	rootView: App
 
+	# --------------------------------------------------------------------------
+	# Laravel Mix Command
+	# --------------------------------------------------------------------------
+	#
+	# Command that runs to execute Laravel Mix when Formidable is in development
+	# mode.
+
+	mix: "npm run mix:watch"
+
 }
--- formidable/vendor/ts/config/inertia.ts
+++ formidable/vendor/ts/config/inertia.ts
@@ -10,6 +10,16 @@ export default {
 	 * Sets the root template that's loaded on the first page visit.
 	 */
 
-	rootView: App
+	rootView: App,
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Laravel Mix Command
+	 * --------------------------------------------------------------------------
+	 * Command that runs to execute Laravel Mix when Formidable is in development
+	 * mode.
+	 */
+
+	mix: "npm run mix:watch"
 
 }
