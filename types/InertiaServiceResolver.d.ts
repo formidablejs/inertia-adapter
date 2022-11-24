b1537917fd6e339ab99bf199db56d72bed592b0d
Donald Pakkies <donaldpakkies@gmail.com>
2022-11-24 20:17:14 +0200

    feat: build imba and gen types

--- types/InertiaServiceResolver.d.ts
+++ types/InertiaServiceResolver.d.ts
@@ -1,4 +1,9 @@
 export class InertiaServiceResolver extends ServiceResolver {
     boot(): import("@formidablejs/framework").Application;
+    enableInertia(): import("@formidablejs/framework").Application;
+    /**
+    @param {string} script
+    */
+    enableMix(script: string): import("@formidablejs/framework").Application;
 }
 import { ServiceResolver } from "@formidablejs/framework";
