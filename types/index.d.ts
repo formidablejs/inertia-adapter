7eae78d5bbd49d06730bafa39f2a3749f10cffd3
Donald Pakkies <donaldpakkies@gmail.com>
2023-03-18 15:46:59 +0200

    feat: set loadash and axios to global window

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,14 @@ import { InertiaRedirect } from "./InertiaRedirect";
 import { InertiaResponse } from "./InertiaResponse";
 import { InertiaServiceResolver } from "./InertiaServiceResolver";
 import { version } from "./Support/Helpers";
+import type { LoDashStatic } from 'lodash';
+import type { Axios } from 'axios';
+
+declare global {
+    interface Window {
+        _: LoDashStatic,
+		axios: Axios
+    }
+}
+
 export { Inertia, InertiaMiddleware, InertiaRedirect, InertiaResponse, InertiaServiceResolver, version };
